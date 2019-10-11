4fc8c876379a6df10f5891c22f13ddc4d1e88f0e
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2019-10-11 08:35:43 +0200

    done with task 1.2 Least squares with a linear basis function model

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,5 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    exp_x = np.array([x, ] * (degree + 1))
+    return np.power(exp_x.T, range(degree + 1))
